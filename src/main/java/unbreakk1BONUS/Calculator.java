100cf5aba41bd22555d40afb5b2ce1920f8eaa31
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-05 15:20:47 +0100

    add calc bonus

---
+++ src/main/java/unbreakk1BONUS/Calculator.java
@@ -0,0 +1,17 @@
+﻿package unbreakk1BONUS;
+
+public class Calculator
+{
+    private Calculator() {} // don't you dare try to call me, I'm busy!
+
+
+    //<edit-fold>desc="INT STUFF">
+    public static int add(int a, int b) {return a + b;}
+
+    public static int subtract(int a, int b) {return a - b;}
+
+    public static int multiply(int a, int b) {return a * b;}
+
+    public static int divide(int a, int b) {return a / b;}
+
+}
---
+++ src/main/java/unbreakk1BONUS/Main.java
@@ -0,0 +1,15 @@
+﻿package unbreakk1BONUS;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 5;
+
+        System.out.println("add: " + Calculator.add(a, b));
+        System.out.println("subtract: " + Calculator.subtract(a, b));
+        System.out.println("multiply: " + Calculator.multiply(a, b));
+        System.out.println("divide: " + Calculator.divide(a, b));
+    }
+}
